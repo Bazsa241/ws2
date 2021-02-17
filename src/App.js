17185375b769951e34df73e4e79e7def7f3bea82
Bazsa241 <horvathbalazsdev@gmail.com>
2021-02-17 14:53:10 +0100

    ws2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import data from "./data/data"
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+
+  const [employees, setEmployess] = useState(data)
+  const [howManyEmployees, setHowManyEmployees] = useState([])
+  const [showCreate, setShowCreate] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const deleteEmployee = (id) => {
+    setEmployess(employees.filter(item => item.id !== id))
+  }
+
+  const incCount = () => {
+    if(count < employees.length)setCount(count + 1)
+  }
+
+  const decCount = () => {
+    if(count > 0) setCount(count - 1)
+  }
+
+  useEffect(() => {
+    setHowManyEmployees(employees.slice(0,count))
+  }, [count])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employees</h1>
+      <button onClick={incCount}>plus</button>
+      <div style={{"fontSize": "5rem"}}>{count}</div>
+      <button onClick={decCount}>minus</button>
+      <br/>
+      <button onClick={() => setShowCreate(!showCreate)}>
+        {
+          showCreate
+          ? "Close"
+          : "Add"
+        }
+      </button>
+      {
+        showCreate &&
+          <CreateEmployee 
+            setEmployess={setEmployess}
+            employees={employees}
+          />
+      }
+      <ul>
+      {
+        howManyEmployees.map(item => <Employee 
+          {...item}
+          key={item.id}
+          deleteEmployee={deleteEmployee}
+          />)
+      }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+const Employee = ({firstName, position, join, deleteEmployee, id}) => {
+
+  const mainDivStyle = {
+    "border": "1px solid white",
+    "margin": "20px 0",
+    "listStyle": "none",
+  }
+
+  return (
+    <li style={mainDivStyle}>
+      <h2>{firstName}</h2>
+      <p>{position}</p>
+      <p>{join}</p>
+      <button onClick={() => deleteEmployee(id)}>
+        Delete
+      </button>
+      <button>
+        Edit
+      </button>
+    </li>
+  )
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const CreateEmployee = ({setEmployess, employees}) => {
+
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [position, setPosition] = useState("")
+  const [born, setBorn] = useState("")
+
+  const newEmployee = () => {
+    const id = Date.now()
+    const join = new Date().toISOString().slice(0, 10)
+
+    setEmployess([...employees, {id, join, firstName, lastName, position, born}])
+
+    setFirstName("")
+    setLastName("")
+    setPosition("")
+    setBorn("")    
+  }
+
+  return (
+    <div>
+      <input type="text" placeholder="First Name"
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <input type="text" placeholder="Lase Name"
+             value={lastName}
+             onChange={e => setLastName(e.target.value)}
+      />
+      <input type="text" placeholder="Position"
+             value={position}
+             onChange={e => setPosition(e.target.value)}
+      />
+      <input type="date"
+             value={born}
+             onChange={e => setBorn(e.target.value)}
+      />
+      <button onClick={newEmployee}>create</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+const employees = [
+  {
+    id: 0,
+    firstName: "Gabriella",
+    lastName: "Inez",
+    born: "1978-05-23",
+    join: "2019-03-12",
+    position: "HR"
+  },
+  {
+    id: 1,
+    firstName: "Rosalind ",
+    lastName: "Neal",
+    born: "1989-04-13",
+    join: "2020-02-11",
+    position: "Engineering"
+  },
+  {
+    id: 2,
+    firstName: "Teri ",
+    lastName: "Binder",
+    born: "1988-12-10",
+    join: "2019-05-13",
+    position: "Security"
+  },
+  {
+    id: 3,
+    firstName: "Megan ",
+    lastName: "Newton",
+    born: "1990-01-30",
+    join: "2021-01-10",
+    position: "Psychologist"
+  },
+  {
+    id: 4,
+    firstName: "Ellery  ",
+    lastName: "Wright",
+    born: "1992-06-07",
+    join: "2019-03-12",
+    position: "Engineering"
+  },
+  {
+    id: 5,
+    firstName: "Darell ",
+    lastName: "Barker",
+    born: "1995-05-23",
+    join: "2020-08-22",
+    position: "HR"
+  },
+  {
+    id: 6,
+    firstName: "Madeline ",
+    lastName: "Griffith",
+    born: "2000-04-11",
+    join: "2021-02-05",
+    position: "Engineering"
+  },
+  {
+    id: 7,
+    firstName: "Vernon ",
+    lastName: "Cook",
+    born: "1997-11-02",
+    join: "2019-09-25",
+    position: "Security"
+  },
+  {
+    id: 8,
+    firstName: "Grant ",
+    lastName: "Ward",
+    born: "1989-07-30",
+    join: "2019-04-11",
+    position: "HR"
+  },
+  {
+    id: 9,
+    firstName: "Oliver ",
+    lastName: "Windrow",
+    born: "1998-03-29",
+    join: "2020-11-02",
+    position: "Engineering"
+  },
+  {
+    id: 10,
+    firstName: "Stan ",
+    lastName: "Hayward",
+    born: "1999-12-23",
+    join: "2020-07-30",
+    position: "Accountant"
+  }
+]
+
+export default employees
